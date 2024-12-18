324c902ed166183c9c650f61a1b9726eee747a18
reedfreling <reed.freling@gmail.com>
2024-12-17 22:29:58 -0500

    Terrible 2048 skeleton
    Body: This is bad
    
    Testing Instructions:
        1. Testing in browser

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-function addItem() {
-  const ul = document.querySelector('ul');
-  const listItem = document.createElement('li');
-  // listItem.innerHTML = '<h1>Hello</h1>';
-  listItem.appendChild(document.createTextNode("Hello"));
-  ul.appendChild(listItem);
+boxes = document.querySelectorAll(".square");
+function getRandomSquare() {
+  return Math.floor(Math.random() * 16);
 }
 
-const button = document.querySelector('#main-button');
-button.addEventListener('mouseover', e => {
-  const paragraph = document.createElement('p');
-  paragraph.classList.add('latest-paragraph');
-  paragraph.innerText = 'A new mouseover';
-  document.querySelector('body').appendChild(paragraph);
+function initializeRandomSquares() {
+  const first = getRandomSquare();
+  let second = getRandomSquare();
+  while (second === first) {
+    second = getRandomSquare();
+  }
+  const firstBox = boxes[first];
+  const secondBox = boxes[second];
+  const firstBoxNumber = document.createElement('h3');
+  firstBoxNumber.innerText = '2';
+  firstBoxNumber.style = 'text-align: center; color: gray; vertical-align: baseline;';
+  const secondBoxNumber = document.createElement('h3');
+  secondBoxNumber.innerText = '2';
+  secondBoxNumber.style = 'text-align: center; color: gray; vertical-align: baseline;';
+  firstBox.appendChild(firstBoxNumber);
+  secondBox.appendChild(secondBoxNumber);
+}
+
+initializeRandomSquares();
+
+document.addEventListener('keydown', e => {
+  console.log(e);
+  switch (e.key) {
+    case 'ArrowLeft':
+      console.log('left');
+      break;
+    case 'ArrowRight':
+      console.log('right');
+      break;
+    case 'ArrowDown':
+      console.log('down');
+      break;
+    case 'ArrowUp':
+      console.log('up');
+      break;
+  }
 });
